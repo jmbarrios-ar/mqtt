cd3f16c5da3d9a55ede8861d34de830858e84545
Juan Manuel Barrios <jmbarrios.ar@outlook.com>
2024-02-27 10:33:57 -0300

    mas esp8266

--- mqtt-esp8266-prueba/mqtt-esp8266-prueba/mqtt-esp8266-prueba.ino
+++ mqtt-esp8266-prueba/mqtt-esp8266-prueba/mqtt-esp8266-prueba.ino
@@ -2,18 +2,24 @@
 #include <PubSubClient.h>
 
 // WiFi
-const char *ssid = "247IASbros2.4"; // Enter your WiFi name
-const char *password = "T3reKByo2023$";  // Enter WiFi password
+//const char *ssid = "247IASbros2.4"; // Enter your WiFi name
+//const char *password = "T3reKByo2023$";  // Enter WiFi password
+const char *ssid = "datacenter"; // Enter your WiFi name
+const char *password = "NOv22$1nicI0";  // Enter WiFi password
 
 // MQTT Broker
 const char *mqtt_broker = "45.186.124.70";
-const char *topic = "esp8266/test";
+const char *topic = "datacenter/noc/alarma";
 const char *mqtt_username = "admin";
 const char *mqtt_password = "Ia$247forever";
 const int mqtt_port = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+//Alarm
+int inputPin = D7; // for ESP8266 microcontroller
+//int inputPin = 4; // for ESP32 microcontroller
 
 void setup() {
   // Set software serial baud to 115200;
@@ -43,6 +49,10 @@ void setup() {
   // publish and subscribe
   client.publish(topic, "hola Juancito");
   client.subscribe(topic);
+  //Entrada para los eventos de alarma
+  pinMode(inputPin, INPUT);
+  Serial.begin(9600);
+  delay(60*1000);
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
@@ -56,6 +66,16 @@ void callback(char *topic, byte *payload, unsigned int length) {
   Serial.println("-----------------------");
 }
 
-void loop() {
+void loop() 
+{
   client.loop();
+  int val = digitalRead(inputPin);
+  if (val == HIGH) {
+    Serial.println("Motion detected!");
+    }
+  else {
+    Serial.println("No Motion detected!");
+    }
+    
+  delay(10);
 }
